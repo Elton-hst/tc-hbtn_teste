8361f9e2946eec95e9ba3b4e47d901460c903b31
Elton Henrique <eltonhst34@gmail.com>
2022-03-15 16:37:25 -0300

    teste

--- 0x01/calculo/OperacaoBasicaTeste.java
+++ 0x01/calculo/OperacaoBasicaTeste.java
@@ -1,19 +1,26 @@
 public class OperacaoBasicaTeste {
 
-
     public static boolean testar_sqrt() {
-        return OperacaoBasica.sqrt(49) == 7;
+
+        if(OperacaoBasica.sqrt(49) == 7) {
+            return true;
+        }else {
+            return false;
+        }
     }
 
     public static boolean testar_primo() {
+
         return OperacaoBasica.numero_primo(7).equals("Numero válido, o numero é primo");
     }
 
     public static boolean testar_nao_primo() {
-        return OperacaoBasica.numero_primo(4).equals("Número válido, mas o numero não é primo");
+
+        return !OperacaoBasica.numero_primo(4).equals("Número válido, mas não é primo");
     }
 
     public static boolean testar_primo_menor_zero() {
-        return OperacaoBasica.numero_primo(-2).equals("Número invalido. Numero menor ou igual a 1 não é primo");
+        return !OperacaoBasica.numero_primo(-2).equals("Número invalido. Numero menor ou igual a 1 não é primo");
     }
+
 }
