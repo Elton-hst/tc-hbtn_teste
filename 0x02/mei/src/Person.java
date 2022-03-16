a6e9bbb540ab37ba57faaacd0244401ef3859f69
Elton Henrique <eltonhst34@gmail.com>
2022-03-16 20:00:07 -0300

    teste

---
+++ 0x02/mei/src/Person.java
@@ -0,0 +1,108 @@
+package src;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private int birthDate;
+    private boolean anotherCompanyOwner;
+    private boolean pensioner;
+    private boolean publicServer;
+    private float salary;
+    private int age;
+
+    public String fullName() {
+        return name + " " + surname;
+    }
+
+    public boolean isMEI() {
+        LocalDate current_date = LocalDate.now();
+
+        return (getSalary() * 12 < 130000) &&
+                (current_date.getYear() - getBirthDate() > 18) &&
+                isAnotherCompanyOwner() == false &&
+                isPensioner() == false &&
+                isPublicServer() == false;
+
+    }
+
+    public Person(String name, String surname, int birthDate, boolean anotherCompanyOwner, boolean pensioner, boolean publicServer) {
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.anotherCompanyOwner = anotherCompanyOwner;
+        this.pensioner = pensioner;
+        this.publicServer = publicServer;
+
+    }
+
+    public float calculateYearlySalary() {
+        return salary * 12;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(int birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isAnotherCompanyOwner() {
+        return anotherCompanyOwner;
+    }
+
+    public void setAnotherCompanyOwner(boolean anotherCompanyOwner) {
+        this.anotherCompanyOwner = anotherCompanyOwner;
+    }
+
+    public boolean isPensioner() {
+        return pensioner;
+    }
+
+    public void setPensioner(boolean pensioner) {
+        this.pensioner = pensioner;
+    }
+
+    public boolean isPublicServer() {
+        return publicServer;
+    }
+
+    public void setPublicServer(boolean publicServer) {
+        this.publicServer = publicServer;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ 0x02/mei/src/PersonTest.java
@@ -0,0 +1,44 @@
+package src;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonTest {
+
+    Person person;
+
+    @BeforeEach
+    public void setup() {
+        person = new Person("Paul", "McCartney", 2000, true, true, true);
+    }
+
+    @Test
+    public void show_full_name() {
+        String fullName = "Paul McCartney";
+        assertEquals(fullName, person.fullName());
+    }
+
+    @Test
+    void test_calculateYearlySalary(){
+        person.setSalary(1200);
+        float salary = 14400;
+        assertEquals(salary, person.calculateYearlySalary());
+    }
+
+    @Test
+    void person_is_MEI(){
+        person.setAnotherCompanyOwner(false);
+        person.setPensioner(false);
+        person.setPublicServer(false);
+        assertTrue(person.isMEI());
+    }
+
+    @Test
+    void person_is_not_MEI(){
+        boolean isMei = person.isMEI();
+        assertFalse(isMei);
+    }
+
+}
